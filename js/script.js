d71229ba22f5b7a0315dd37e085a4f5800b6a033
Fleibson <fleytecinformatica@gmail.com>
2026-02-19 00:08:36 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -57,5 +57,14 @@ menuToggle.addEventListener("click", function () {
     navMenu.classList.toggle("active");
 });
 
+document.addEventListener("DOMContentLoaded", function () {
 
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("active");
+    });
+
+});
 
